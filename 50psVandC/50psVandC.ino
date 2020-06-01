6885702440bc352608dfba1044a8e7f8fd991370
paul.carr-at-544611607123 <paul.w.carr@gmail.com>
2020-06-01 16:27:31 +0100

    changed MAC

--- 50psVandC/50psVandC.ino
+++ 50psVandC/50psVandC.ino
@@ -25,7 +25,7 @@ EnergyMonitor grid;             // Create an instance
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xAD
 };
 IPAddress ip(192, 168, 1, 177);
 
