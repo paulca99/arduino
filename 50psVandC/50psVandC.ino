746453d6a4dc76f7ce72c19f135c600cb7c61572
paul.carr-at-544611607123 <paul.w.carr@gmail.com>
2020-06-02 20:58:41 +0100

    added web and display

--- 50psVandC/50psVandC.ino
+++ 50psVandC/50psVandC.ino
@@ -22,7 +22,40 @@
 #include "EmonLib.h"             // Include Emon Library
 EnergyMonitor grid;             // Gridmon
 EnergyMonitor solar;            // Solarmon
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000
+};
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -74,6 +107,25 @@ void setup() {
   grid.current(1, 49.9);
   solar.voltage(2, 224.26, 1.7);  // Voltage: input pin, calibration, phase_shift
   solar.current(3, 49.9);
+  //*********init DISPLAY
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
 }
 
 
@@ -122,5 +174,30 @@ void loop() {
     // close the connection:
     client.stop();
     Serial.println("client disconnected");
+  } else
+  {
+    Serial.print("GRID : ");
+    grid.calcVI(20, 500);        // Calculate all. No.of half wavelengths (crossings), time-out
+    grid.serialprint();
+    Serial.print("SOLAR : ");
+    solar.calcVI(20, 500); // Calculate all. No.of half wavelengths (crossings), time-out
+    solar.serialprint();
+    int gridp = (int)grid.realPower;
+    int solarp = (int)solar.realPower;
+
+    display.clearDisplay();
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 2);
+    // Display static text
+    display.println("GRD:" + String(gridp));
+    display.setCursor(0, 22);
+    display.println("SLR:" + String(solarp));
+    display.setCursor(0, 42);
+    int homePower = (int)(solar.realPower - grid.realPower);
+    display.println("HME:" + String(homePower));
+    display.display();
+    delay(500);
   }
 }
