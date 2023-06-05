e8ae017abaec23e1cf710a963b57969485094a62
Paul Carr <paul.w.carr@gmail.com>
2023-06-05 13:19:41 +0100

    get gti on for first brew

--- esp32/charger/timestuff.cpp
+++ esp32/charger/timestuff.cpp
@@ -6,34 +6,31 @@
 extern bool GTIenabled;
 struct tm timeinfo;
 const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
-const int   daylightOffset_sec = 3600;
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 3600;
 
-int GTI_on_time=6;
-int GTI_off_time=22;
-void timeSetup()
-{
+int GTI_on_time = 6;
+int GTI_off_time = 22;
+void timeSetup() {
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 }
 
-void timeLoop()
-{
+void timeLoop() {
 
-  if(!getLocalTime(&timeinfo)){
+  if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
     return;
-  }
-  else
-  {
+  } else {
     Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-    if ((timeinfo.tm_hour >GTI_on_time) && (timeinfo.tm_hour < GTI_off_time))
-    {
-      GTIenabled=true;
-    }
-    else
-    {
-      GTIenabled=false;
-      turnGTIOff();
+    if ((timeinfo.tm_hour >= GTI_on_time) && (timeinfo.tm_hour < GTI_off_time) && GTIenabled == false) {
+      GTIenabled = true;
+      turnPowerOn();
+      turnPowerOff();
+    } else {
+      if (GTIenabled == true) {
+        GTIenabled = false;
+        turnGTIOff();
+      }
     }
   }
 }
